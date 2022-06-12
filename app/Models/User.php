9a1bd0e3b0ac22441189b50f61154a8de95d2b48
JackWorld <jackwong0326@gmail.com>
2022-06-12 19:56:57 +0800

    Add user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,17 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function __construct(array $attributes=[]){
+        parent::__construct(attributes);
+        self::creatde(function (User $user){
+            if(!$user->roles()->get()->contains(2)){
+                $user->roles()->attach(2);
+            }
+        });
+    }
+
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
