acf93da1be0814667d09e1ef11a67ba3998c835a
JackWorld <jackwong0326@gmail.com>
2022-06-14 23:13:32 +0800

    Add middleware auth for route.

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
-Route::group(['middleware'=>'auth'],function(){
-    Route::resource('tasks',App\Http\Controllers\TaskController::class);
-    Route::resource('users',App\Http\Controllers\UserController::class);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('tasks', \App\Http\Controllers\TasksController::class);
+    Route::resource('users', \App\Http\Controllers\UsersController::class);
 });
 
