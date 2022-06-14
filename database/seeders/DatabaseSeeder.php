66e8eeead970db924ea131db2f6850cabb8db355
JackWorld <jackwong0326@gmail.com>
2022-06-14 23:11:41 +0800

    Add default data with seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            PermissionTableSeeder::class,
+            RoleTableSeeder::class,
+            PermissionTableSeeder::class,
+            UserTableSeeder::class,
+            RoleUserTableSeeder::class,
+        ]);
     }
 }
