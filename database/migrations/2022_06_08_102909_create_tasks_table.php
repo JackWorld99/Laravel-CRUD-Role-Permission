7732abd78a166fb4bc7a8ae325fbef6c8568bd2d
JackWorld <jackwong0326@gmail.com>
2022-06-14 23:07:26 +0800

    Fix typo.

--- database/migrations/2022_06_08_102909_create_tasks_table.php
+++ database/migrations/2022_06_08_102909_create_tasks_table.php
@@ -17,6 +17,7 @@ class CreateTasksTable extends Migration
             $table->id();
             $table->string('description');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
